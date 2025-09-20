881bad2c59e9f489c30430ce3eb4a782debe8b78
ibadullah-18 <Husey_iz52@itstep.edu.az>
2025-09-20 19:56:30 +0400

    SP 03

---
+++ SP EV_TAP 03/Program.cs
@@ -0,0 +1,38 @@
+﻿using CancellationTokenSource cts = new();
+CancellationToken cancellationToken = cts.Token;
+
+
+ThreadPool.QueueUserWorkItem(a =>
+{
+    try
+    {
+        Download(cancellationToken);
+    }
+    catch (OperationCanceledException ex)
+    {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine("Yukleme prosesi dayandirildi!");
+    }
+});
+
+var key = Console.ReadKey();
+
+if (key.Key == ConsoleKey.Enter)
+{
+    cts.Cancel();
+    Thread.Sleep(1000);
+}
+Console.ReadKey();
+void Download(CancellationToken token)
+{
+    Console.WriteLine("Yukleme baslayir...");
+    Thread.Sleep(1000);
+    for (int i = 0; i < 100; i++)
+    {
+        token.ThrowIfCancellationRequested();
+        Console.WriteLine($"{i}%");
+        Thread.Sleep(10);
+        Console.Clear();
+    }
+    Console.WriteLine("Yukleme bitdi...");
+}
